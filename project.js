40ec297437bf8a966dcefa15fa56037bbbd2955b
lyonska <146982728+lyonska@users.noreply.github.com>
2023-10-09 13:27:44 -0400

    Update project.js

--- project.js
+++ project.js
@@ -1,5 +1,12 @@
-const hi = document.querySelector(".hi");
+function changeColor(){
+    document.querySelector('.box1').style.color = 'blue';
+  }
 
-function changeColor() {
-    hi.style.color = "blue";
-}
+  function changeTheme(){
+    document.querySelector('.header').style.color = 'orange';
+    document.querySelector('.header').style.background = 'black';
+    document.querySelector('.navbar').style.background= 'orange';
+    document.querySelector('footer').style.color= 'orange';
+    document.querySelector('footer').style.background= 'black';
+
+  }
